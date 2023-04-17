e524116c2a702f9f9585d01fa7d42ef9171cd68e
gabrielsantiag0 <gabrielleandrosantiago9@gmail.com.br>
2023-04-17 12:29:31 -0300

    falta o envio de email com nodemailer e terminar a home e o sobre nos

--- Js/FaleConosco.js
+++ Js/FaleConosco.js
@@ -1,3 +1,34 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express()
+
+const port = 3000
+const user = 'chufogodechao@outlook.com';
+const pass = 'Crismora40';
+
+app.get('/send',(req, res) =>{
+
+    const transporter = nodemailer.createTransport({
+        Host : 'smtp.office365.com',
+        Port : '587',
+        auth: {user, pass}
+        // SMTPSecure : 'STARTTLS',
+        // SMTPAuth : 'true'
+    })
+    transporter.sendMail({
+        from : user,
+        to: user,
+        replyTo: user,
+        subject: "ola seja bem vindo",
+        text: "olá ",
+    }).then(info=>{
+        res.send(info)
+    }).catch(error =>{
+        res.send(error)
+    })
+})
+
+//===================================
 const form = document.getElementById('FaleConosco');
 const nameInput = document.getElementById('nome');
 const emailInput = document.getElementById('email');
