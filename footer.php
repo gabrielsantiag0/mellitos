e86ec6f283a36dac85e7413c2d896606cfdb4519
gabrielsantiag0 <117419017+gabrielsantiag0@users.noreply.github.com>
2023-04-22 12:47:18 -0300

    mudei o nome de html para phhp

---
+++ footer.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/receita.css">
+    <title>Receitas</title>
+</head>
+<body>
+    <!-- rodapé -->
+    <footer class="bg-danger mt-5">
+      <div class="row text-center">    
+        <div class="col-4">
+          <div class="row">
+              <div class="col">
+                <div class="col d-block p-5">
+                <ul>
+                  <h6 class="pt-3 text-white text-decoration-underline">
+                    LINKS
+                  </h6>
+                  <li class="text-white text-center">
+                    <h6> <a class="nav-link" href="FaleConosco.html">Contato</a></h6>
+                    <h6> <a class="nav-link" href="download.html">Download</a></h6>
+                    <h6> <a class="nav-link" href="receita.html">Receitas</a></h6>
+                    <h6> <a class="nav-link" href="https://diabetesplay.com.br/">Videos</a></h6>
+                    <h6> <a class="nav-link" href="index.html">Home</a></h6>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col">
+                <div class="col d-block">
+                  <div class="mt-5">
+                  <h6 class="p-3"><a href="FaleConosco.html" class="text-white">FALE-CONOSCO</a></h6>
+                </div>
+                <div class="text-white">
+                  Telefone: (11) 2185-9200
+                </div> <br>
+                <div>
+                  <p class="text-white fs-5">Nossos Canais:</p>
+                  <div class="row d-flex justify-content-center">
+                      <div class="d-flex justify-content-evenly">
+                        <a class="img-fluid" href="https://www.instagram.com/"><img src="Img/instagramIcone.png" alt="" width="40px"></a>
+                        <a class="img-fluid" href="https://pt-br.facebook.com/"><img src="Img/facebook.png" alt="" width="40px"></a>
+                        <a class="img-fluid" href="https://twitter.com/i/flow/login"><img src="Img/twitter.png" alt="" width="40px"></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+            <div class="col-8">
+        <div class="d-block">
+         <a href="index.html">
+          <img src="Img/logoPequena2.jpg" alt="" class="mt-3 border border-white border-1 rounded rounded-3 mb-3">
+         </a>
+          <div class="text-white">
+              <h5>
+                “Acesso aos Cuidados - se não agora, quando?”: 14/11 - <br> Dia Mundial e Nacional do Diabetes
+              </h5>
+          </div>
+          <div>
+            <p class="text-white">
+              O acesso à educação sobre diabetes é o foco do mês de conscientização <br> sobre o diabetes e do Dia Mundial do Diabetes em novembro.
+  
+             Todos os anos, a campanha <br> do Dia Mundial do Diabetes se concentra em um tema dedicado que dura um ou mais anos. <br>
+              O tema do Dia Mundial do Diabetes 2021-23 é Acesso aos Cuidados com o Diabetes .
+            </p>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="row text-center me-5">
+        <h6 class="text-white ms-5">Desenvolvido por Equipe Acesso Dm Mellitus&trade; 2023</h6>
+        </div>
+        <button class="voltar-ao-topo btn bg-black m-5 fs-3">&uarr;</button>
+     </footer>   
+</body>
+<script src="Js/estilo.js"></script>
+</html>
